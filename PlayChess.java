ef5aa00d78466f06e72269a3f3e11e86ff2accdb
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-09-26 20:28:30 -0500

    Initial commit

---
+++ PlayChess.java
@@ -0,0 +1,9 @@
+public class PlayChess {
+	public static void main(String[] args){
+		ChessBoard board = new ChessBoard();
+		ChessPiece whitePieces = new ChessPiece[16];
+		ChessPiece blackPieces = new ChessPiece[16];
+		
+	}
+
+}
